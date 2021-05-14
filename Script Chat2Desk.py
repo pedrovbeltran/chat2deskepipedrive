c35d9eb840264f17e57364fe838c4c6039566345
Pedro Beltran <pedrovbeltran@gmail.com>
2021-05-14 20:27:56 -0300

    Successful integration using requests in Python

--- Script Chat2Desk.py
+++ Script Chat2Desk.py
@@ -1,12 +1,67 @@
+#API_KEY = 27467b9a2417db30f514d4b1a945ff
+# Client Example = 175220815
+#https://companydomain.pipedrive.com/api/v1/deals/2?api_token=659c9fddb16335e48cc67114694b52074e812e03
+
+import re, sys, time, json
+from datetime import datetime
+from requests import get, put, post, delete
+
 class Handler:
   #
   # new message handler
   #
+  def add_client(self, input_data, c2d):
+    # Cliente novo, adicionando no Pipedrive
+    new_client = {'name': input_data['client']['name'], 'phone': input_data['client']['phone']}
+    obj = post(api_url+'persons?api_token='+api_token, json=new_client)
+    client = json.loads(obj.text)
+    print(client)
+    return client
+
+  def post_add_lead(self, data, input_data, c2d):
+    # Adicionando lead
+    new_lead = {'title':'Lead '+data['name'], 'person_id':data['id']}
+    obj = post(api_url+'leads?api_token='+api_token, json=new_lead)
+    lead = json.loads(obj.text)
+    return lead
+
+  def adiciona_lead(self, input_data, c2d):
+    obj = get(api_url+'persons/search?term=' + input_data['client']['name'] +'&fields=name&start=0&limit=2&api_token='+api_token)
+    person = json.loads(obj.text)
+    if len(person["data"]['items']) == 0:
+      client = self.add_client(input_data, c2d)
+
+      if client['success'] == 0:
+        return 'Ocorreu um erro adicionando o cliente'
+      
+      return self.post_add_lead(client['data'], input_data, c2d)
+
+    elif len(person['data']['items'] > 1):
+      obj = get(api_url+'persons/search?term=' + input_data['client']['phone'][-9::] +'&fields=phone&start=0&limit=1&api_token='+api_token)
+      person = json.loads(obj.text)
+      if len(person["data"]['items']) == 0:
+        client = self.add_client(input_data, c2d)
+    
+        if client['success'] == false:
+          return 'Ocorreu um erro adicionando o cliente'
+          
+        return self.post_add_lead(client['data']['owner_id'], input_data, c2d)
+    
+    else:
+      # Caso para cliente ja existente
+      return ' '
+  
   def new_message_handler(self, input_data, c2d):
     # This returns client id for every message received. The id is sent in system message, which is not seen by the client. To enable, turn on 'Incoming messages' option above.
     # Use this id for receiving logs of your script: text to connected messenger account and see your id, which you have to enter into Logging section below.
-    c2d.send_message(input_data['client']['id'], "This client's id: " + str(input_data['client']['id']) + ".\n\n To turn off this message go to Settings/Script (under admin) and turn off 'Incoming messages' option.", "system")
-    return input_data
+    #api_headers=c2d.token
+    #c2d.send_message(input_data['client']['id'], "This client's id: " + str(input_data['client']['id']) + ".\n\n To turn off this message go to Settings/Script (under admin) and turn off 'Incoming messages' option.", "system")
+    data = c2d.get_client_info(input_data['client']['id'])
+    if len(data['tags']) == 0:
+        response = self.adiciona_lead(input_data, c2d)
+        return response
+    else:
+        return data['tags']
 
   #
   # before sending message handler
@@ -81,6 +136,9 @@ class Handler:
   def delete_tag_from_request_handler(self, input_data, c2d):
     return '[delete_tag_from_request] do logic here'
 
+api_url = 'https://edb.pipedrive.com/api/v1/'
+api_token = '1e244e5c6eb88d91d95d3fdb865f53c45683f46c'
+
 # examples
 # send message
 #response = c2d.send_message(94212, 'test!!!')
@@ -126,4 +184,9 @@ class Handler:
 
 # not send menu in new_message_handler add
 # print 'not send menu'
+        
+        
+        
+        
+        
         
